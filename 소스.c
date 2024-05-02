33e5b8a34715dac90575323705d65e860c02b0d1
jy241 <jy241@naver.com>
2024-05-02 10:31:59 +0900

    큐

--- 소스.c
+++ 소스.c
@@ -1,85 +1,144 @@
 #include <stdio.h>
-#define MAX_TERMS 101
+#include <stdlib.h>
+#include <string.h>
 
-struct {
-	float coef;
-	int expon;
-} terms[MAX_TERMS] = { {8.0, 3},{7.0, 1}, {1.0, 0},
-						{10.0,3}, {3.0, 2}, {1.0, 0} }; //index 5까지 사용
+#define MAX_QUEUE_SIZE 100
+#define MAX_STACK_SIZE 100
 
-int avail = 6; //index 6부터 비어있음
-void print_poly(int s, int e);
-void poly_add2(int As, int Ae, int Bs, int Be, int* Csp, int* Cep);
+typedef int element;
+
+typedef struct {
+    element data[MAX_QUEUE_SIZE];
+    int front, rear;
+} QueueType;
+
+typedef struct {
+    int data[MAX_STACK_SIZE];
+    int top;
+} Stack;
+
+// 스택 초기화 함수
+void init_stack(Stack* s) {
+    s->top = -1;
+}
+
+// 스택이 비어있는지 확인하는 함수
+int is_empty(Stack* s) {
+    return (s->top == -1);
+}
+
+// 스택에 데이터를 추가하는 함수
+void push(Stack* s, int item) {
+    if (s->top < MAX_STACK_SIZE - 1) {
+        s->data[++(s->top)] = item;
+    }
+    else {
+        fprintf(stderr, "스택이 가득 찼습니다.\n");
+        exit(1);
+    }
+}
+
+// 스택에서 데이터를 꺼내는 함수
+int pop(Stack* s) {
+    if (!is_empty(s)) {
+        return s->data[(s->top)--];
+    }
+    else {
+        fprintf(stderr, "스택이 비어있습니다.\n");
+        exit(1);
+    }
+}
+
+// 큐 초기화 함수
+void init_queue(QueueType* q) {
+    q->front = q->rear = 0;
+}
+
+// 큐가 비어있는지 확인하는 함수
+int is_empty_queue(QueueType* q) {
+    return (q->front == q->rear);
+}
+
+// 큐가 가득 찼는지 확인하는 함수
+int is_full(QueueType* q) {
+    return ((q->rear + 1) % MAX_QUEUE_SIZE == q->front);
+}
+
+// 큐에 데이터를 추가하는 함수
+void enqueue(QueueType* q, int item) {
+    if (is_full(q)) {
+        fprintf(stderr, "큐가 가득 찼습니다.\n");
+        exit(1);
+    }
+    q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
+    q->data[q->rear] = item;
+}
+
+// 큐에서 데이터를 꺼내는 함수
+int dequeue(QueueType* q) {
+    if (is_empty_queue(q)) {
+        fprintf(stderr, "큐가 비어있습니다.\n");
+        exit(1);
+    }
+    q->front = (q->front + 1) % MAX_QUEUE_SIZE;
+    return q->data[q->front];
+}
+
+// 회문 검사 함수
+int is_palindrome(char* str) {
+    Stack s;
+    QueueType q;
+    int len = strlen(str);
+
+    init_stack(&s);
+    init_queue(&q);
+
+    // 문자열을 스택과 큐에 각각 저장
+    for (int i = 0; i < len; i++) {
+        push(&s, str[i]);
+        enqueue(&q, str[i]);
+    }
+
+    // 스택과 큐에서 문자를 하나씩 꺼내면서 비교
+    while (!is_empty(&s) && !is_empty_queue(&q)) {
+        if (pop(&s) != dequeue(&q)) {
+            return 0; // 회문이 아님
+        }
+    }
+
+    return 1; // 회문임
+}
 
 int main() {
+    char str[100];
+    int choice;
 
-	int Cs, Ce;
-	printf("A = ");
-	print_poly(0, 2);
-	printf("B = ");
-	print_poly(3, 5);
+    while (1) {
+        printf("1. 회문 입력\n");
+        printf("2. 회문 검사\n");
+        printf("3. 종료\n");
+        printf("메뉴 선택: ");
+        scanf_s("%d", &choice);
+        while (getchar() != '\n');
 
-	poly_add2(0, 2, 3, 5, &Cs, &Ce);
-	printf("A+B = ");
-	print_poly(Cs, Ce);
-}
-
-/* void attach(float coef, int expon) {
-	if (avail > MAX_TERMS) {
-		print(stderr, "Too long terms\n");
-		exit(1);
-	}
-	terms[avail].coef = coef;
-	terms[avail].expon = expon;
-} */
-
-void poly_add2(int As, int Ae, int Bs, int Be, int *Csp, int *Cep){
-	int c_start = avail;
-
-	while ((As <= Ae) && (Bs <= Be)) {
-		if (terms[As].expon > terms[Bs].expon) {
-			terms[c_start].expon = terms[As].expon;
-			terms[c_start].coef = terms[As].coef + terms[Bs].coef;
-			//attach(terms[As].coef, terms[As].expon); //간단하게 요약 가능
-			As++;
-			c_start++;
-		}
-		else if (terms[As].expon == terms[Bs].expon) {
-			terms[c_start].expon = terms[As].expon;
-			terms[c_start].coef = terms[As].coef + terms[Bs].coef;
-			As++;
-			Bs++;
-			c_start++;
-		}
-
-		else {
-			terms[c_start].expon = terms[Bs].expon;
-			terms[c_start].coef = terms[Bs].coef;
-			Bs++;
-			c_start++;
-		}
-	for (; As <= Ae; As++) {
-		terms[c_start].expon = terms[As].expon;
-		terms[c_start].coef = terms[As].coef;
-		As++;
-		c_start++;
-	}
-	for (; Bs <= Be; Bs++) {
-		terms[c_start].expon = terms[Bs].expon;
-		terms[c_start].coef = terms[Bs].coef;
-		Bs++;
-		c_start++;
-	}
-	*Csp = avail; //c_start
-	*Cep = c_start - 1; //avail -1 
-	avail = c_start; //지움 //c_start++ 다 지워야함?ㅌ	
-	} 
-
-}
-
-void print_poly(int s, int e) {
-	for (int i = s; i < e; i++) {
-		printf("%3.1fx^%d + ", terms[i].coef, terms[i].expon);
-	}
-	printf("%3.1f\n", terms[e].coef); //printf("%3.1fx^%d + ", terms[e].coef, terms[e].expon);
+        switch (choice) {
+        case 1:
+            printf("회문을 입력하세요: ");
+            scanf_s("%[^\n]s", str);
+            while (getchar() != '\n');
+            break;
+        case 2:
+            if (is_palindrome(str))
+                printf("입력한 문자열은 회문입니다.\n");
+            else
+                printf("입력한 문자열은 회문이 아닙니다.\n");
+            break;
+        case 3:
+            exit(0);
+        default:
+            printf("잘못된 메뉴 선택\n");
+            break;
+        }
+    }
+    return 0;
 }
